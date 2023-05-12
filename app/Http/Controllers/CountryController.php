d3b401d15c5e264bbc33b34316a1d830a850bb95
MikiDani <miklosdani@gmail.com>
2023-05-12 19:23:51 +0200

    app_02

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\CountryModel;
+
+class CountryController extends Controller
+{
+    public function country(){
+        return response()->json(CountryModel::get(), 200);
+    }
+
+    public function countryByID($id){
+        return response()->json(CountryModel::find($id), 200);
+    }
+
+    public function countrySave(Request $request){
+        $country = CountryModel::create($request->all());
+        return response()->json($country, 201);
+    }
+}
